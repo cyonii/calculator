5d6cb95386bccfd0844889f508f220ef0d621bac
CY Kalu <cy23@tutanota.com>
2021-07-10 13:32:27 +0000

    Style calculator card

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,12 @@ const App = () => (
     <h1 className="mt-3 mb-4">Calculator</h1>
     <div className="row justify-content-center">
       <div className="col-sm-8 col-md-4">
-        <Display />
-        <ButtonPanel />
+        <div className="card calc-card border-0">
+          <div className="card-body py-5">
+            <Display />
+            <ButtonPanel />
+          </div>
+        </div>
       </div>
     </div>
   </div>
