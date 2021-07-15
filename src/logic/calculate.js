489831a524e10662ce87df35063a5013b3ed8abc
CY Kalu <cy23@tutanota.com>
2021-07-15 18:28:53 +0000

    Handle user actions

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,20 +1,47 @@
 import operate from './operate';
 import { resetState } from '../state/utils';
 
-export default function calculate(data, btnName) {
-  let { total, next, operation } = { ...data };
+// Helper to resolve numeric input
+const numberResolver = (initial, addition) => {
+  if (addition === '.') {
+    if (initial.toString().includes('.')) return initial;
+    return initial + addition;
+  }
+  return (initial === 0 || initial === '0') ? addition : (initial + addition);
+};
 
-  if (btnName === 'AC') { // To reset state
-    ({ total, next, operation } = resetState());
-  } else if (btnName === '+/-') { // To negate one of the operands
+export default function calculate(data, buttonName) {
+  let { total, next, operator } = { ...data };
+  const OPERATORS = ['÷', '*', '+', '-', '%'];
+
+  // Reset state with AC key
+  if (buttonName === 'AC') return resetState();
+
+  if (buttonName.match(/[0-9]|\./) !== null) { // When number key or dot (.) is pressed
+    if (!operator) {
+      total = numberResolver(total, buttonName);
+    } else {
+      next = numberResolver(next, buttonName);
+    }
+  } else if (OPERATORS.includes(buttonName)) { // When an operator is pressed
+    if (buttonName === '%') {
+      total = operate(total, next, buttonName);
+    } else {
+      operator = buttonName;
+    }
+  } else if (buttonName === '+/-') { // When '+/-' => should negate current input
     if (next) {
       next *= -1;
-    } else if (total) {
+    } else {
       total *= -1;
     }
-  } else if (btnName === '=') { // To run calculation operation
-    operate(total, next, operation);
+  } else if (buttonName === '=') { // When '=' is pressed => run calculation
+    if (total && next && operator) {
+      total = operate(total, next, operator).toString();
+      next = '';
+      operator = '';
+    }
   }
 
-  return { total, next, operation };
+  return { total, next, operator };
 }
