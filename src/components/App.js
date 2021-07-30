0d8bc5043f9acbc55edfbd2188775cedb8987cb7
CY Kalu <cy23@tutanota.com>
2021-07-30 15:24:05 +0000

    Rename state file

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
-import { resetState } from '../state/utils';
+import { resetState } from '../state/state';
 
 class App extends React.Component {
   constructor(props) {
